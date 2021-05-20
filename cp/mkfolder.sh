61a7a1ed195c36d503052be3fee6e926a9470800
ritwik1923 <leonofindia.com>
2021-05-21 00:44:43 +0530

    update mkfolder script

--- cp/mkfolder.sh
+++ cp/mkfolder.sh
@@ -1,16 +1,19 @@
-# echo "path: "
-# read p
-# echo $p
-# cd $p
-# pwd
+
 echo "how many practice: "
 read s
+echo "Starting index: "
+read start
+date=$(date '+%d-%m-%Y')
+echo $date
+mkdir $date
+cd $date
+
 checkFolder() {
 	dir=$1
 	# echo $dir
 	
-	echo "$p/$dir"
-if [ -d "$p/$dir" ]; then
+	echo "$dir"
+if [ -d "$dir" ]; then
    return 1
    else 
    return 0
@@ -22,19 +25,39 @@ fi
 for ((i=0;i<s;++i)); 
 do
 	# echo "$s $i"
-	folder=$((800+$i*100))
+	folder=$(($start+$i*100))
 	checkFolder $folder
 	ret=$?
 	echo $ret
-	# if [ $ret == 1 ];
-	# then
-	# 	echo "$folder acha.."
-	# else 
-	# 	mkdir $folder
-	# 	echo "$folder made"
-	# fi
+	if [ $ret == 1 ];
+	then
+		echo "$folder acha.."
+	else 
+		mkdir $folder
+		echo "$folder made"
+	fi
 	
 done
 
-cd "800" 
-code .
+# echo "entering $start"  
+while [ true ]
+do
+   echo "Enter folder in $date ('e' to exit)"
+   read folder
+   if [ "$folder" = 'e' ]
+   then 
+   echo "Exiting kaka!!.. :P"
+    break
+   else
+        echo "Entering $folder"
+		cd $folder
+		code .
+   fi
+
+done
+
+# start .
+# open $start
+
+# remove after testing
+# rmdir $date
