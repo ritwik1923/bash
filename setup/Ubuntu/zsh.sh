fb36b5327c31d047e23027d9eceb464bd9178b56
Ritwik Mandal <63177644+ritwik1923@users.noreply.github.com>
2022-01-10 23:16:41 +0530

    Create zsh.sh

---
+++ setup/Ubuntu/zsh.sh
@@ -0,0 +1,4 @@
+sudo apt install git vim curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
